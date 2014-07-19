5d02aeaf8a637af4f963d8da6d20a613d9e08131
jakrog10 <jakrog10@hotmail.com>
2014-07-19 14:39:14 -0400

    add a couple classes

---
+++ Model/Destination.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class Destination
+    {
+        public int DestinationId { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+
+        public byte[] Photo { get; set; }
+
+        public List<Lodging> Lodgings { get; set; }
+
+    }
+}
---
+++ Model/Lodging.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class Lodging
+    {
+        public int LodgingId { get; set; }
+
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public bool IsResort { get; set; }
+
+        public Destination Destination { get; set; }
+    }
+}
